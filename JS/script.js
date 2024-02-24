4bb5cb0da88acdfc4459c4a978496a55811fbbfe
DarleneLL <153782176+DarleneLL@users.noreply.github.com>
2024-02-24 17:24:42 -0300

    Exercicio 8

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,15 @@
 function menssagem(){
     alert("Seja bem-vindo");
 }
+
+let texto = document.getElementById("texto")
+let textoOrig = texto.innerHTML
+
+texto.addEventListener("mouseover", function(){
+    texto.innerHTML= "Minha biografia"
+})
+
+texto.addEventListener("mouseout", function(){
+    texto.innerHTML= textoOrig
+})
+
